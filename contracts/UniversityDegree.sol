c17a116e9be92e8ecb68495035afdb5d72931bb7
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-02-04 15:19:47 +0100

    UniversityDegree.sol implementation & degree.json example

--- contracts/UniversityDegree.sol
+++ contracts/UniversityDegree.sol
@@ -5,6 +5,7 @@ import "./dependancies/ERC721URIStorage.sol";
 import "./dependancies/librairies/Counters.sol";
 
 error UniversityDegree__NotOwner();
+error UniversityDegree__YourDegreeNotIssued();
 
 /**
  * @title UniversityDegree
@@ -13,10 +14,15 @@ error UniversityDegree__NotOwner();
  * @dev
  */
 contract UniversityDegree is ERC721URIStorage {
+    // ERC721 Variables:
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    address private immutable i_owner;
+    address private immutable i_owner; // Issuer of degrees
+
+    // Degree Variables:
+    mapping(address => bool) public issuedDegrees;
+    mapping(address => string) public personToDegree;
 
     modifier onlyOwner() {
         if (msg.sender != i_owner) {
@@ -27,6 +33,32 @@ contract UniversityDegree is ERC721URIStorage {
 
     constructor() ERC721("SoulBoundToken", "SBT") {
         i_owner = msg.sender;
+    }
+
+    function issueDegree(address to) external onlyOwner {
+        issuedDegrees[to] = true;
+    }
+
+    function claimDegree(string memory tokenURI) public returns (uint256) {
+        if (!issuedDegrees[msg.sender]) {
+            revert UniversityDegree__YourDegreeNotIssued();
+        }
+
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        issuedDegrees[msg.sender] = false;
+        personToDegree[msg.sender] = tokenURI;
+
+        return newItemId;
+    }
+
+    function checkDegreeOfPerson(
+        address person
+    ) external view returns (string memory) {
+        return personToDegree[person];
     }
 
     function getOwner() public view returns (address) {
