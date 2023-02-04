03f3b67c7a74bf1ba43be870a8c3f26ac89ceefe
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-02-04 18:21:15 +0100

    Making the nft 100% on-chain with cutomisable parameters

--- contracts/UniversityDegree.sol
+++ contracts/UniversityDegree.sol
@@ -13,7 +13,11 @@ error UniversityDegree__ScoreTooHigh();
  * @title UniversityDegree
  * @author Ghaieth BEN MOUSSA
  * @notice Non-transferable Soul Bound Token (NFT) smart contract for university degrees and diplomas
- * @dev
+ * @dev This contract is used to award degrees to students who graduated in two steps:
+ * 1. Only the owner of the contract can issue degrees with the function `issueDegree(address to, uint256 score)`
+ * 2. Then, the student can claim the degree with the function `claimDegree()`
+ * This type of token is called a Soul Bound Token because once the token 
+ * has been claimed by the student, it can not be transfered to anyone else.
  */
 contract UniversityDegree is ERC721URIStorage {
     using Strings for uint256;
@@ -23,13 +27,13 @@ contract UniversityDegree is ERC721URIStorage {
 
     // Degree Variables:
     address private immutable i_owner; // Issuer of the degrees
-    uint256 public s_degreeMaxScore; // Maximum score of the degree hosted ob IPFS
-    string public s_degreeImage; // Image of the degree hosted ob IPFS
-    string public s_degreeMajor; // Major of the dgree
-    string public s_degreeType; // Type of the degree
-    mapping(address => bool) public issuedDegrees;
-    mapping(address => uint256) public personToScore;
-    mapping(address => string) public personToDegree;
+    uint256 internal s_degreeMaxScore; // Maximum score of the degree
+    string internal s_degreeImage; // Image of the degree hosted ob IPFS
+    string internal s_degreeMajor; // Major of the degree
+    string internal s_degreeType; // Type of the degree
+    mapping(address => bool) internal s_issuedDegrees;
+    mapping(address => uint256) internal s_personToScore;
+    mapping(address => string) internal s_personToDegree;
 
     modifier onlyOwner() {
         if (msg.sender != i_owner) {
@@ -55,12 +59,12 @@ contract UniversityDegree is ERC721URIStorage {
         if (score > s_degreeMaxScore) {
             revert UniversityDegree__ScoreTooHigh();
         }
-        issuedDegrees[to] = true;
-        personToScore[to] = score;
+        s_issuedDegrees[to] = true;
+        s_personToScore[to] = score;
     }
 
     function claimDegree() public returns (uint256) {
-        if (!issuedDegrees[msg.sender]) {
+        if (!s_issuedDegrees[msg.sender]) {
             revert UniversityDegree__YourDegreeNotIssued();
         }
 
@@ -70,12 +74,12 @@ contract UniversityDegree is ERC721URIStorage {
 
         string memory tokenURI = generateTokenURI(
             newItemId,
-            personToScore[msg.sender]
+            s_personToScore[msg.sender]
         );
         _setTokenURI(newItemId, tokenURI);
 
-        issuedDegrees[msg.sender] = false;
-        personToDegree[msg.sender] = tokenURI;
+        s_issuedDegrees[msg.sender] = false;
+        s_personToDegree[msg.sender] = tokenURI;
 
         return newItemId;
     }
@@ -83,25 +87,25 @@ contract UniversityDegree is ERC721URIStorage {
     function generateTokenURI(
         uint256 tokenId,
         uint256 score
-    ) public view returns (string memory) {
+    ) private view returns (string memory) {
         bytes memory dataURI = abi.encodePacked(
             '{',
-                '"name": "', s_degreeMajor, ' #', tokenId.toString(), '",',
+                '"name": "', s_degreeMajor, ' Degree #', tokenId.toString(), '",',
                 '"image": "', s_degreeImage, '",',
                 '"description": "An award conferred by a college or university signifying that the recipient has satisfactorily completed a course of study",',
                 '"attributes": [',
                     '{',
                         '"trait type": "Score",',
-                        '"value": "', score.toString(), '",',
-                        '"max_value": ', s_degreeMaxScore, '",',
-                    '}',
+                        '"value": ', score.toString(), ',',
+                        '"max_value": ', s_degreeMaxScore.toString(),
+                    '},',
                     '{',
                         '"trait type": "Major",',
-                        '"value": "', s_degreeMajor, '",',
-                    '}',
+                        '"value": "', s_degreeMajor, '"',
+                    '},',
                     '{',
                         '"trait type": "Type of degree",',
-                        '"value": "', s_degreeType, '",',
+                        '"value": "', s_degreeType, '"',
                     '}',
                 ']',
             '}'
@@ -114,13 +118,42 @@ contract UniversityDegree is ERC721URIStorage {
         );
     }
 
-    function checkDegreeOfPerson(
-        address person
+    // Getters
+    function checkDegreeOfStudent(
+        address student
     ) external view returns (string memory) {
-        return personToDegree[person];
+        return s_personToDegree[student];
+    }
+
+    function checkScoreOfStudent(
+        address student
+    ) public view returns (uint256) {
+        return s_personToScore[student];
+    }
+
+    function isStudentDegreeIssued(
+        address student
+    ) public view returns (bool) {
+        return s_issuedDegrees[student];
     }
 
     function getOwner() public view returns (address) {
         return i_owner;
+    }
+
+    function getDegreeMaxScore() public view returns (uint256) {
+        return s_degreeMaxScore;
+    }
+
+    function getDegreeImage() public view returns (string memory) {
+        return s_degreeImage;
+    }
+
+    function getDegreeMajor() public view returns (string memory) {
+        return s_degreeMajor;
+    }
+
+    function getDegreeType() public view returns (string memory) {
+        return s_degreeType;
     }
 }
